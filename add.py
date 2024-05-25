8690862e2ace1c31c85232a5f544705506f53337
shreevidhuran <brillantmakers@gmail.com>
2024-05-25 12:04:42 +0530

    Update add.py
    Changes updated to identifier Num1 as firstNumber Num2 as secondNumber

--- add.py
+++ add.py
@@ -1,8 +1,8 @@
 #Adding Two Numbers
 #Python programm to add two numbers
-Num1 = int(input("Enter the first number : "))
-Num2 = int(input("Enter the second number : "))
+firstNumber = int(input("Enter the first number : "))
+secondNumber = int(input("Enter the second number : "))
 #Adding two numbers
-sum = Num1 + Num2
+sum = firstNumber + secondNumber
 #printing values
-print(f"sum of {Num1} and {Num2} is {sum} ")
+print(f"sum of {firstNumber} and {secondNumber} is {sum} ")
